e6a681e2a89244393195d5fcb3750da9e935a40e
MATHLOUTHI Salma <smathlouthi@ffbb.com>
2019-05-16 11:48:45 +0200

    creation script de deploiement

---
+++ deploy_prod.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#activer mode maintenance
+drush sset system.maintenance_mode = 1
+
+#recuperer le code
+git pull origine matser
+
+
+#ajouter vider le dossier vendor
+
+#reintsaller les librairies
+composer install --no-dev
+
+#vider le cache avant le dump
+drush cr
+
+#faire un backup de la bd
+ drush sql-dump > my-sql-dump-file-name.sql
+
+#Mise à jour de la base
+drush updb -y
+
+#exporter les config de prod a part
+drush csex prod -y
+
+#importer les config de de synch
+drush cim -y
+
+#vider le cache
+drush cr
+
+#desactiver mode maintenance
+drush sset system.maintenance_mode = 0
